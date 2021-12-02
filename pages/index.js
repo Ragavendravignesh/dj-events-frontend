44b5f902ee1531d4ef9289956a853471a9483fe1
Ragavendra S <ragavendrabest77@gmail.com>
2021-12-02 22:17:49 +0530

    Added getStaticProps

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,22 @@
 import Layout from '@/components/Layout'
+import { API_URL } from '@/config/index'
 
-export default function Home() {
+export default function Home({events}) {
+  console.log(events)
   return (
     <Layout title='Dj events | Home page'>
       <h1>This is home page</h1>
     </Layout>
   )
+}
+
+export async function getStaticProps() {
+  const res = await fetch(`${API_URL}/api/events/`);
+
+  const events = await res.json();
+
+  return {
+    props: { events },
+    revalidate: 1
+  }
 }

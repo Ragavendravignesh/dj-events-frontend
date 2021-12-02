407acaad541e8a9b2708d28dcc6b3aeb01df5e41
Ragavendra S <ragavendrabest77@gmail.com>
2021-12-02 23:18:32 +0530

    Added staticPaths and staticProps

--- pages/events/[slug].js
+++ pages/events/[slug].js
@@ -1,9 +1,43 @@
-import Layout from "@/components/Layout";
+import Layout from '@/components/Layout'
+import { API_URL } from '@/config/index'
 
-export default function EventPage() {
-    return (
-        <Layout>
-            <h1>Event page</h1>
-        </Layout>
-    )
+export default function EventPage({ event }) {
+  return (
+    <Layout>
+      <h1>{event.name}e</h1>
+    </Layout>
+  )
 }
+
+export async function getStaticPaths() {
+  const res = await fetch(`${API_URL}/api/events`)
+
+  const events = await res.json()
+
+  const paths = events.map((evt) => ({
+    params: { slug: evt.slug },
+  }))
+
+  return { paths, fallback: true }
+}
+
+export async function getStaticProps({ params: { slug } }) {
+  const res = await fetch(`${API_URL}/api/events/${slug}`)
+
+  const event = await res.json()
+
+  return { props: { event: event[0] }, revalidate: 1 }
+}
+
+// export async function getServerSideProps ({ query : { slug }}) {
+//     const res = await fetch(`${API_URL}/api/events/${slug}`);
+
+//     const event = await res.json();
+//     console.log(event);
+
+//     return {
+//         props : {
+//             event: event[0]
+//         }
+//     }
+// }

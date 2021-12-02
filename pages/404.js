2470a831006fe2de0053f133fb0b362756ed1b7f
Ragavendra S <ragavendrabest77@gmail.com>
2021-12-02 12:27:45 +0530

    Custom error page added

---
+++ pages/404.js
@@ -0,0 +1,23 @@
+import Layout from '../components/Layout'
+import Link from 'next/link'
+import { FaExclamationTriangle } from 'react-icons/fa'
+import styles from '../styles/404.module.css'
+
+export default function PageNotFound() {
+  return (
+    <Layout title='Dj events | Page not found'>
+      <div className={styles.error}>
+        <h1>
+          <FaExclamationTriangle />
+          404
+        </h1>
+        <h4>
+          Sorry, there is nothing here{' '}
+          <Link href='/'>
+            <a>Go back Home</a>
+          </Link>
+        </h4>
+      </div>
+    </Layout>
+  )
+}

d9fa9422507ba3eb72c98ec269b129d04ec5526b
Ragavendra S <ragavendrabest77@gmail.com>
2021-12-05 00:44:56 +0530

    Add event functionality added

--- pages/events/add.js
+++ pages/events/add.js
@@ -1,9 +1,137 @@
-import Layout from "@/components/Layout";
+import Layout from '@/components/Layout'
+import { useState } from 'react'
+import { useRouter } from 'next/router'
+import Link from 'next/link'
+import styles from '@/styles/Form.module.css'
+import { API_URL } from '@/config/index'
+import { ToastContainer, toast } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
 
 export default function AddEvent() {
-    return (
-        <Layout title="DJ events | Add events">
-            <h1>Add event page</h1>
-        </Layout>
+  const router = useRouter()
+  const [values, setValues] = useState({
+    name: '',
+    venue: '',
+    address: '',
+    performers: '',
+    date: '',
+    time: '',
+    description: '',
+  })
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target
+
+    setValues({ ...values, [name]: value })
+  }
+
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+
+    const hasEmptyValues = Object.values(values).some(
+      (element) => element === ''
     )
+
+    if (hasEmptyValues) {
+      toast.error('Please fill all the details')
+    } else {
+        console.log(values);
+      const res = await fetch(`${API_URL}/events`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(values),
+      })
+
+      if (!res.ok) {
+        toast.error('Something went wrong')
+      } else {
+        const evt = await res.json()
+        router.push(`/events/${evt.slug}`)
+      }
+    }
+  }
+
+  return (
+    <Layout title='DJ events | Add events'>
+      <Link href='/events'>Go back</Link>
+      <h1>Add event page</h1>
+      <ToastContainer />
+      <form onSubmit={handleSubmit} className={styles.form}>
+        <div className={styles.grid}>
+          <div>
+            <label htmlFor='name'>Event Name</label>
+            <input
+              type='text'
+              id='name'
+              name='name'
+              value={values.name}
+              onChange={handleInputChange}
+            />
+          </div>
+          <div>
+            <label htmlFor='venue'>Venue</label>
+            <input
+              type='text'
+              id='venue'
+              name='venue'
+              value={values.venue}
+              onChange={handleInputChange}
+            />
+          </div>
+          <div>
+            <label htmlFor='address'>Address</label>
+            <input
+              type='text'
+              id='address'
+              name='address'
+              value={values.address}
+              onChange={handleInputChange}
+            />
+          </div>
+          <div>
+            <label htmlFor='performers'>Performers</label>
+            <input
+              type='text'
+              id='performers'
+              name='performers'
+              value={values.performers}
+              onChange={handleInputChange}
+            />
+          </div>
+          <div>
+            <label htmlFor='date'>Event Date</label>
+            <input
+              type='date'
+              id='date'
+              name='date'
+              value={values.date}
+              onChange={handleInputChange}
+            />
+          </div>
+          <div>
+            <label htmlFor='time'>Event Time</label>
+            <input
+              type='text'
+              id='time'
+              name='time'
+              value={values.time}
+              onChange={handleInputChange}
+            />
+          </div>
+        </div>
+        <div>
+          <label htmlFor='description'>Description</label>
+          <textarea
+            id='description'
+            name='description'
+            value={values.description}
+            onChange={handleInputChange}
+          />
+        </div>
+        <input type='submit' className='btn' value='Add event' />
+      </form>
+    </Layout>
+  )
 }

4dd3c882d18f60a2cf29d92bdfd036e705c5c265
Ragavendra S <ragavendrabest77@gmail.com>
2021-12-07 21:46:35 +0530

    Pagination added

---
+++ components/Pagination.js
@@ -0,0 +1,20 @@
+import Link from 'next/link'
+import { PER_PAGE } from '@/config/index'
+
+export default function Pagination({ page, total }) {
+  return (
+    <>
+      {page > 1 && (
+        <Link href={`/events?page=${page - 1}`}>
+          <a className='btn-secondary'>Prev</a>
+        </Link>
+      )}
+
+      {page < total && (
+        <Link href={`/events?page=${page + 1}`}>
+          <a className='btn-secondary'>Next</a>
+        </Link>
+      )}
+    </>
+  )
+}
--- pages/events/index.js
+++ pages/events/index.js
@@ -1,29 +1,37 @@
 import Layout from '@/components/Layout'
-import { API_URL } from '@/config/index'
-import Link from 'next/link'
-import EventItem from '@/components/EventItem';
+import { API_URL, PER_PAGE } from '@/config/index'
+import EventItem from '@/components/EventItem'
+import Pagination from '@/components/Pagination'
 
-export default function EventsPage({events}) {
+export default function EventsPage({ events, page, total }) {
   return (
     <Layout title='Dj events | Events page'>
       <h1>Events</h1>
 
       {!events.length && <h3>No Events Found</h3>}
 
-      {events.map(evt => (
+      {events.map((evt) => (
         <EventItem key={evt.id} evt={evt} />
       ))}
+
+      <Pagination page={page} total={total} />
     </Layout>
   )
 }
 
-export async function getStaticProps() {
-  const res = await fetch(`${API_URL}/events/`);
+export async function getServerSideProps({ query: { page = 1 } }) {
+  const skip = +page === 0 ? 0 : (+page - 1) * PER_PAGE
+  const res = await fetch(
+    `${API_URL}/events?_sort=date:ASC&_limit=${PER_PAGE}&_start=${skip}`
+  )
+  const events = await res.json()
 
-  const events = await res.json();
+  const totalRes = await fetch(`${API_URL}/events/count`)
+  const totalCount = await totalRes.json()
+
+  const total = Math.ceil(totalCount / PER_PAGE)
 
   return {
-    props: { events },
-    revalidate: 1
+    props: { events, page: +page, total },
   }
 }

f0349266e09cbaf5ef07e3be4aef82f04d0116e9
Ragavendra S <ragavendrabest77@gmail.com>
2021-12-10 10:51:44 +0530

    Getting events for logged in user

--- pages/account/dashboard.js
+++ pages/account/dashboard.js
@@ -1,9 +1,29 @@
 import Layout from '@/components/Layout'
+import { parseCookies } from '@/helpers/index'
+import { API_URL } from '@/config/index'
 
-export default function dashboard() {
+export default function dashboard({ events }) {
+  console.log(events)
   return (
     <Layout title={'Dashboard | DJ events'}>
       <h1>DJ events Dashboard</h1>
     </Layout>
   )
+}
+
+export async function getServerSideProps({ req }) {
+  const { token } = parseCookies(req)
+
+  const strapiRes = await fetch(`${API_URL}/events/me`, {
+    method: 'GET',
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  })
+
+  const events = await strapiRes.json()
+
+  return {
+    props: { events },
+  }
 }
